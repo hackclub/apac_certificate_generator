42d05135157bada0b2beddf476dbf9539a8c2f27
bajpai244 <bajpaiharsh244@gmail.com>
2022-04-06 23:43:37 +0530

    build: use yarn for package management

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /** @format */
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 
-const { App } = require("@slack/bolt");
-const { WebClient, LogLevel } = require("@slack/web-api");
-const { on_mention, on_submit, on_sign } = require("../lib/utlis/index");
-const on_decline = require("../lib/utlis/on_decline");
-const on_file_upload = require("../lib/utlis/on_file_upload");
+const { App } = require('@slack/bolt');
+const { WebClient, LogLevel } = require('@slack/web-api');
+const { on_mention, on_submit, on_sign } = require('../lib/utlis/index');
+const on_decline = require('../lib/utlis/on_decline');
+const on_file_upload = require('../lib/utlis/on_file_upload');
 
-const { oauth_token, app_token } = require("../lib/data");
+const { oauth_token, app_token } = require('../lib/data');
 
 const client = new WebClient(oauth_token, {
   logLevel: LogLevel.DEBUG,
@@ -23,33 +23,35 @@ const app = new App({
 
 (async () => {
   await app.start();
-  console.log("⚡️ Bolt app started");
+  console.log('⚡️ Bolt app started');
+  console.log('apptoken =>', app_token);
+  console.log('oauth token =>', oauth_token);
 })();
 
-app.event("app_mention", async (args) => {
+app.event('app_mention', async (args) => {
   on_mention(args);
 });
 
-app.action("submit", async (args) => {
+app.action('submit', async (args) => {
   on_submit(args);
 });
 
-app.action("sign", async (args) => {
+app.action('sign', async (args) => {
   on_sign(args);
 });
 
-app.action("decline", async (args) => {
+app.action('decline', async (args) => {
   on_decline(args);
 });
 
-app.action("pronoun", async ({ ack }) => {
+app.action('pronoun', async ({ ack }) => {
   await ack();
 });
 
-app.action("user_select", async ({ ack }) => {
+app.action('user_select', async ({ ack }) => {
   await ack();
 });
 
-app.message("", async (args) => {
+app.message('', async (args) => {
   on_file_upload(args);
 });
